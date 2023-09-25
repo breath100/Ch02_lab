fe4743293b1341b3215b8c747732481e575deb46
breath100 <113489083+breath100@users.noreply.github.com>
2023-09-26 02:51:38 +0800

    Add files via upload

---
+++ 2-3-2.sql
@@ -0,0 +1,7 @@
+Create table Department(
+Dep_code char(4),
+Dep_name nvarchar(10) Not null,
+Dep_head nvarchar(10) Not null,
+Primary key(Dep_code)
+)
+
---
+++ 2-3-ex01.sql
@@ -0,0 +1,6 @@
+Create table Student(
+stu_id CHAR(5),
+stu_name nvarchar(10) Not null,
+dep_code CHAR(4) Not null,
+Primary key(stu_id)
+)
